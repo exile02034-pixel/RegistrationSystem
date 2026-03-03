87154b13228ab3e39af76e8265df43017449dc90
Jancee <jancee1capati@gmail.com>
2026-03-03 16:23:26 +0800

    hehe

--- app/Http/Requests/Admin/GenerateRegistrationDocumentRequest.php
+++ app/Http/Requests/Admin/GenerateRegistrationDocumentRequest.php
@@ -60,46 +60,7 @@ class GenerateRegistrationDocumentRequest extends FormRequest
         }
 
         if ($documentType === 'gis_stock_corporation') {
-            return array_merge($rules, [
-                'fields.step_1' => ['required', 'array'],
-                'fields.step_1.corporate_name' => ['required', 'string', 'max:255'],
-                'fields.step_1.sec_registration_number' => ['required', 'string', 'max:255'],
-                'fields.step_1.principal_office_address' => ['required', 'string', 'max:1000'],
-                'fields.step_1.business_address' => ['required', 'string', 'max:1000'],
-                'fields.step_1.email' => ['required', 'email', 'max:255'],
-                'fields.step_1.telephone' => ['nullable', 'string', 'max:50', 'regex:/^\+?[0-9()\-.\s]{6,20}$/'],
-                'fields.step_1.meeting_date_annual' => ['nullable', 'date'],
-                'fields.step_1.meeting_date_special' => ['nullable', 'date'],
-                'fields.step_2' => ['required', 'array'],
-                'fields.step_2.amla_covered' => ['required', 'boolean'],
-                'fields.step_2.amla_reporting_entity' => ['required', 'boolean'],
-                'fields.step_2.amla_other_details' => ['nullable', 'string', 'max:1000'],
-                'fields.step_3' => ['required', 'array'],
-                'fields.step_3.authorized_capital_stock' => ['required', 'numeric', 'min:0'],
-                'fields.step_3.subscribed_capital_stock' => ['required', 'numeric', 'min:0'],
-                'fields.step_3.paid_up_capital_stock' => ['required', 'numeric', 'min:0'],
-                'fields.step_4' => ['required', 'array', 'min:1'],
-                'fields.step_4.*.name' => ['required', 'string', 'max:255'],
-                'fields.step_4.*.nationality' => ['required', 'string', 'max:100'],
-                'fields.step_4.*.shareholdings' => ['required', 'numeric', 'min:0'],
-                'fields.step_5' => ['required', 'array', 'min:1'],
-                'fields.step_5.*.position' => ['required', 'string', 'max:255'],
-                'fields.step_5.*.name' => ['required', 'string', 'max:255'],
-                'fields.step_5.*.tin' => ['nullable', 'string', 'max:50', 'regex:/^\d{3}-\d{3}-\d{3}(?:-\d{3,4})?$/'],
-                'fields.step_6' => ['required', 'array', 'min:1'],
-                'fields.step_6.*.stockholder_name' => ['required', 'string', 'max:255'],
-                'fields.step_6.*.shares' => ['required', 'numeric', 'min:0'],
-                'fields.step_7' => ['required', 'array'],
-                'fields.step_7.external_auditor_name' => ['nullable', 'string', 'max:255'],
-                'fields.step_7.external_auditor_tin' => ['nullable', 'string', 'max:50', 'regex:/^\d{3}-\d{3}-\d{3}(?:-\d{3,4})?$/'],
-                'fields.step_8' => ['required', 'array'],
-                'fields.step_8.corporate_secretary_name' => ['required', 'string', 'max:255'],
-                'fields.step_8.corporate_secretary_tin' => ['nullable', 'string', 'max:50', 'regex:/^\d{3}-\d{3}-\d{3}(?:-\d{3,4})?$/'],
-                'fields.step_9' => ['required', 'array'],
-                'fields.step_9.certifier_name' => ['required', 'string', 'max:255'],
-                'fields.step_9.certifier_tin' => ['required', 'string', 'max:50', 'regex:/^\d{3}-\d{3}-\d{3}(?:-\d{3,4})?$/'],
-                'fields.step_9.certifier_date' => ['required', 'date'],
-            ]);
+            return $rules;
         }
 
         return [
